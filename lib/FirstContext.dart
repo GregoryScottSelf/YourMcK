6c94937152ce170d75c0d066a7825624d16d4f85
Gregory Scott Self <46310761+GregoryScottSelf@users.noreply.github.com>
2019-02-11 23:34:45 -0600

    Add files via upload

--- lib/FirstContext.dart
+++ lib/FirstContext.dart
@@ -1,14 +1,97 @@
 import'package:flutter/material.dart';
-class FirstContext extends StatelessWidget {
+import'package:cloud_firestore/cloud_firestore.dart';
+class FirstContext extends StatefulWidget {
+  @override
+  _FirstContext createState() => _FirstContext ();
+
+}
+class _FirstContext extends State<FirstContext> {
 
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text(
-        "First Content",
-        style: new TextStyle(fontSize: 20.0),
-      ),
+    return new Scaffold(
+
+        body: new StreamBuilder(
+          stream: Firestore.instance.collection("Event").where("GroupName",isEqualTo:"UPE").snapshots(),
+          builder: (context,snapshot){
+            if(!snapshot.hasData)
+              return new Text("Rendering...");
+            return new ListView.builder(
+                itemCount: snapshot.data.documents.length,
+                itemBuilder: (context,index){
+                  DocumentSnapshot ds=snapshot.data.documents[index];
+                  return buildResultCard(ds);
+                }
+            );
+          },
+        ),
+
+
     );
 
   }
+
+
+
+}
+Widget buildResultCard(ds) {
+
+  return Center(
+      child: Card(
+
+          child: Container(
+              child:Column(
+                 mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:<Widget>[
+                    Text(ds['GroupName']+" just posted!"+"\n"+ds['EventTitle']+"\n"+ds['Where']+" @ "+ds['Time'],
+                      textAlign: TextAlign.center,
+
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+
+                      ),
+                    ),
+                    ButtonTheme.bar(
+
+                        child:ButtonBar(
+                            mainAxisSize: MainAxisSize.max,
+                          alignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            FlatButton(
+
+                              child: const Text('Maybe'),
+
+                              onPressed:(){},
+
+                            ),
+                            Icon(
+                                Icons.not_listed_location,
+
+
+                            ),
+                            FlatButton(
+
+                              child: const Text("Skip"),
+                              onPressed:(){},
+                            ),
+                            Icon(Icons.clear),
+
+                            FlatButton(
+                              child:  Text("Going"),
+                              onPressed: (){},
+                            ),
+                            Icon(Icons.check_box),
+
+
+                          ],
+                        )
+                    )
+                  ]
+              )
+          )
+      )
+  );
+
 }
